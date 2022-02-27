d3e5cb91744128664335c874601b91def6ecaaf2
rmurat <rony.murat@gmail.com>
2022-02-27 16:25:40 +0100

    more robust main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 import numpy as np
 
-from typing import List
+from typing import Any, List, Optional
 from doggydo import doggy
 from doggydo.doggy import DoggyOrder
 
@@ -17,26 +17,32 @@ def clamp_detections(detections: List[DoggyOrder], limit: int = 5) -> List[Doggy
 
 def get_order_given(last_detections: List[DoggyOrder]) -> DoggyOrder:
     """Returns the order to give as regard of all the detections given"""
+    for order in DoggyOrder:
+        if all(order == detection for detection in last_detections):
+            return order
     return DoggyOrder.NONE
 
 
-def get_new_detection(frame: np.ndarray) -> DoggyOrder:
-    """Run the prediction and return the Detection observed"""
+def get_new_detection(frame: np.ndarray, mode: Optional[Any]) -> DoggyOrder:
+    """Run the prediction on the given frame and return the order observed"""
     return DoggyOrder.NONE
 
 
 def main():
+    # Init vars and load models here
     last_detections = []
+    order_detection_model = None
 
     if not doggy.start():
         raise RuntimeError("Doggy did not start!")
 
+    # Main event loop
     while True:
         frame = doggy.get_camera_frame()
         if frame is not None:
-            new_detection = get_new_detection(frame)
+            new_detection = get_new_detection(frame, order_detection_model)
             last_detections.append(new_detection)
-            last_detections = clamp_detections(last_detections)
+            last_detections = clamp_detections(last_detections, limit=5)
             current_order = get_order_given(last_detections)
 
             if current_order != DoggyOrder.NONE and doggy.ready():
